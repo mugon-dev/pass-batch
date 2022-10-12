0942385694ab38519a2e02bc2b17c804d779fa08
Conny <jmg41490400@gmail.com>
2022-10-12 17:47:16 +0900

    SB-13 package entity, repository 추가

---
+++ src/main/java/com/fastcampus/pass/repository/packaze/PackageRepository.java
@@ -0,0 +1,7 @@
+package com.fastcampus.pass.repository.packaze;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PackageRepository extends JpaRepository<PackageEntity, Integer> {
+
+}
